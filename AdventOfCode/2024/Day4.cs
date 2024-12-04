9b2dcbc5cb2969d8cc96ce410e2c06f5bc7ccc39
Shawna Dawson <sndawson@ualberta.ca>
2024-12-03 23:56:10 -0700

    Day 4 Part 1
    This is so unhinged. Also won't work at all for part 2, so going to
    have to do a different, better approach for that.

---
+++ AdventOfCode/2024/Day4.cs
@@ -0,0 +1,133 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2024
+{
+    public class Day4 : IDay
+    {
+        public int Part1(List<string> input)
+        {
+            var occurrences = 0;
+            //start by just finding the occurrences on one line
+            foreach (var line in input)
+            {
+                occurrences += CountXMASMatches(line);
+            }
+            // certainly not the most efficient, but then figure out all the other lines?
+            foreach (var line in GetVerticalLines(input))
+            {
+                occurrences += CountXMASMatches(line);
+            }
+            foreach (var line in GetTopLeftDownDiagonalLines(input))
+            {
+                occurrences += CountXMASMatches(line);
+            }
+            foreach (var line in GetBottomLeftUpDiagonalLines(input))
+            {
+                occurrences += CountXMASMatches(line);
+            }
+
+            return occurrences;
+        }
+
+        public int Part2(List<string> input)
+        {
+            return 0;
+        }
+
+        private int CountXMASMatches(string input)
+        {
+            var xmasMatches = Regex.Matches(input, @"XMAS").Count;
+            var samxMatches = Regex.Matches(input, @"SAMX").Count;
+            return xmasMatches + samxMatches;
+        }
+
+        private List<string> GetVerticalLines(List<string> input)
+        {
+            var verticalLines = new List<string>(new string[input.Count]);
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    // char array would be more efficient so it's not recreating the string
+                    verticalLines[j] += input[i][j];
+                }
+            }
+            return verticalLines;
+        }
+
+        private List<string> GetTopLeftDownDiagonalLines(List<string> input)
+        {
+            var numberOfDiagonalLines = input.Count + input[0].Length - 1;
+            var diagonalLines = new List<string>(new string [numberOfDiagonalLines]);
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (i == 0)
+                {
+                    for (int j = 0; j < input[i].Length; j++)
+                    {
+                        int a = i;
+                        int b = j;
+                        while (a < input.Count && b < input[i].Length)
+                        {
+                            diagonalLines[i + j] += input[a][b];
+                            a++;
+                            b++;
+                        }
+                    }
+                }
+                else
+                {
+                    int a = i;
+                    int b = 0;
+                    while (a < input.Count && b < input[i].Length)
+                    {
+                        diagonalLines[i + input[i].Length - 1] += input[a][b];
+                        a++;
+                        b++;
+                    }
+                }
+            }
+
+            return diagonalLines;
+        }
+
+        private List<string> GetBottomLeftUpDiagonalLines(List<string> input)
+        {
+            var numberOfDiagonalLines = input.Count + input[0].Length - 1;
+            var diagonalLines = new List<string>(new string[numberOfDiagonalLines]);
+
+            for (int i = input.Count - 1; i >= 0; i--)
+            {
+                if (i == input.Count - 1)
+                {
+                    for (int j = 0; j < input[i].Length; j++)
+                    {
+                        int a = i;
+                        int b = j;
+                        while (a >= 0 && b < input[i].Length)
+                        {
+                            diagonalLines[i + j] += input[a][b];
+                            a--;
+                            b++;
+                        }
+                    }
+                }
+                else
+                {
+                    int a = i;
+                    int b = 0;
+                    while (a >= 0 && b < input[i].Length)
+                    {
+                        diagonalLines[i] += input[a][b];
+                        a--;
+                        b++;
+                    }
+                }
+            }
+
+            return diagonalLines;
+        }
+
+    }
+}
---
+++ AdventOfCodeTests/2024/Day4Tests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using AdventOfCode._2024;
+using AdventOfCode;
+
+namespace AdventOfCodeTests
+{
+    public class Day4Tests
+    {
+        readonly IDay day = new Day4();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        private static List<string> sampleInput1 = new List<string>
+        {
+            "MMMSXXMASM",
+            "MSAMXMSMSA",
+            "AMXSXMAAMM",
+            "MSAMASMSMX",
+            "XMASAMXAMM",
+            "XXAMMXXAMA",
+            "SMSMSASXSS",
+            "SAXAMASAAA",
+            "MAMMMXMMMM",
+            "MXMXAXMASX",
+        };
+
+        private static List<string> sampleInput2 = new List<string>
+        {
+            ""
+        };
+
+        private static readonly object[] inputTestCasesPart1 =
+        {
+            new object[]
+            {
+                sampleInput1,
+                18
+            },
+        };
+
+        private static readonly object[] inputTestCasesPart2 =
+        {
+            new object[]
+            {
+                sampleInput2,
+                0
+            },
+        };
+
+        [TestCaseSource("inputTestCasesPart1")]
+        public void TestPart1(List<string> input, int solution)
+        {
+            Assert.AreEqual(solution, day.Part1(input));
+        }
+
+        [TestCaseSource("inputTestCasesPart2")]
+        public void TestPart2(List<string> input, int solution)
+        {
+            Assert.AreEqual(solution, day.Part2(input));
+        }
+    }
+}
