b40bea526a48d823669980efe04d1a436c6e939b
Shawna Dawson <sndawson@ualberta.ca>
2024-12-05 23:14:11 -0700

    WIP: Day 6 Part 1
    The test passes on the sample input, but I'm not getting the correct
    answer with my puzzle input yet. No idea what the problem is yet.

---
+++ AdventOfCode/2024/Day6.cs
@@ -0,0 +1,142 @@
+﻿namespace AdventOfCode._2024
+{
+    public class Day6 : IDay
+    {
+        List<char> arrows = new List<char> { '^', '<', '>', 'v' };
+
+        public int Part1(List<string> input)
+        {
+            // first find the guard
+            var guardCoordinates = FindGuardCoordinates(input);
+
+            // then walk the guard's path until they leave the area, marking the visited tiles
+            var trackingBoard = CreateTrackingBoard(input);
+            //var areaMapCopy = new char[10, 10];
+            var i = guardCoordinates.Item1;
+            var j = guardCoordinates.Item2;
+            var guardDirection = input[i][j];
+            while (i < input.Count && i > 0 && j < input[j].Length && j > 0)
+            {
+                // first mark that we've been here
+                trackingBoard[i][j] = 'X';
+
+                (int, int) nextTile;
+                switch (guardDirection)
+                {
+                    case '^':
+                        nextTile = (i - 1, j);
+                        break;
+                    case '<':
+                        nextTile = (i, j - 1);
+                        break;
+                    case '>':
+                        nextTile = (i, j + 1);
+                        break;
+                    case 'v':
+                        nextTile = (i + 1, j);
+                        break;
+                    default:
+                        throw new Exception($"Illegal direction: {guardDirection}");
+                }
+
+                // the while loop was supposed to handle this, but I didn't set this up properly for that
+                char nextTileValue;
+                try
+                {
+                    nextTileValue = input[nextTile.Item1][nextTile.Item2];
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    // the guard has left the area, so stop processing
+                    break;
+                }
+                
+
+                if (nextTileValue == '.' || arrows.Contains(nextTileValue))
+                {
+                    // go to next tile
+                    i = nextTile.Item1;
+                    j = nextTile.Item2;
+                }
+                else if (nextTileValue == '#')
+                {
+                    // obstacle, turn to the right
+                    switch (guardDirection)
+                    {
+                        case '^':
+                            guardDirection = '>';
+                            break;
+                        case '<':
+                            guardDirection = '^';
+                            break;
+                        case '>':
+                            guardDirection = 'v';
+                            break;
+                        case 'v':
+                            guardDirection = '<';
+                            break;
+                        default:
+                            throw new Exception($"Illegal direction: {guardDirection}");
+                    }
+                }
+                else
+                {
+                    throw new Exception($"Illegal tile value: ${nextTileValue}");
+                }
+
+            }
+
+            // finally count the number of visited tiles
+            var guardPositions = 0;
+            foreach (var row in trackingBoard)
+            {
+                foreach (var tile in row)
+                {
+                    Console.Write(tile.ToString());
+                    if (tile == 'X')
+                    {
+                        guardPositions++;
+                    }
+                }
+                Console.Write("\n");
+            }
+
+            return guardPositions;
+        }
+
+        public int Part2(List<string> input)
+        {
+            return 0;
+        }
+
+        private (int, int) FindGuardCoordinates(List<string> input)
+        {
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    if (arrows.Contains(input[i][j]))
+                    {
+                        return (i, j);
+                    }
+                }
+            }
+            return (-1, -1);
+        }
+
+        private List<List<char>> CreateTrackingBoard(List<string> input)
+        {
+            // I'm certain there are better ways to do this
+            var board = new List<List<char>>();
+            for (int i = 0; i < input.Count; i++)
+            {
+                board.Add(new List<char>());
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    board[i].Add('.');
+                }
+            }
+            return board;
+        }
+    }
+}
---
+++ AdventOfCodeTests/2024/Day6Tests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using AdventOfCode._2024;
+using AdventOfCode;
+
+namespace AdventOfCodeTests
+{
+    public class Day6Tests
+    {
+        readonly IDay day = new Day6();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        private static List<string> sampleInput1 = new List<string>
+        {
+            "....#.....",
+            ".........#",
+            "..........",
+            "..#.......",
+            ".......#..",
+            "..........",
+            ".#..^.....",
+            "........#.",
+            "#.........",
+            "......#...",
+        };
+
+        private static List<string> sampleInput2 = new List<string>
+        {
+            ""
+        };
+
+        private static readonly object[] inputTestCasesPart1 =
+        {
+            new object[]
+            {
+                sampleInput1,
+                41
+            },
+        };
+
+        private static readonly object[] inputTestCasesPart2 =
+        {
+            new object[]
+            {
+                sampleInput2,
+                0
+            },
+        };
+
+        [TestCaseSource("inputTestCasesPart1")]
+        public void TestPart1(List<string> input, int solution)
+        {
+            Assert.AreEqual(solution, day.Part1(input));
+        }
+
+        [TestCaseSource("inputTestCasesPart2")]
+        public void TestPart2(List<string> input, int solution)
+        {
+            Assert.AreEqual(solution, day.Part2(input));
+        }
+    }
+}
