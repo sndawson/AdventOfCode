c914366564dbf56445c149edfcd3b91a39d0ab4e
Shawna Dawson <sndawson@ualberta.ca>
2024-12-06 23:57:48 -0700

    Day 7
    My IDay interface can't handle non-int returns, so had to print the
    result in a weird way.

---
+++ AdventOfCode/2024/Day7.cs
@@ -0,0 +1,99 @@
+﻿namespace AdventOfCode._2024
+{
+    public class Day7 : IDay
+    {
+        public int Part1(List<string> input)
+        {
+            var result = Part1Long(input);
+            Console.WriteLine(result.ToString());
+            return (int)result;
+        }
+
+        public long Part1Long(List<string> input)
+        {
+            long totalCalibrationResult = 0;
+
+            foreach (var line in input)
+            {
+                var parts = line.Split(": ");
+                var testResult = long.Parse(parts[0]);
+                var testValues = parts[1].Split(' ').Select(x => long.Parse(x)).ToList();
+
+                if (IsValidEquation(testResult, null, testValues))
+                {
+                    totalCalibrationResult += testResult;
+                }
+            }
+
+            return totalCalibrationResult;
+        }
+
+        public int Part2(List<string> input)
+        {
+            var result = Part2Long(input);
+            Console.WriteLine(result.ToString());
+            return (int)result;
+        }
+
+        public long Part2Long(List<string> input)
+        {
+            long totalCalibrationResult = 0;
+
+            foreach (var line in input)
+            {
+                var parts = line.Split(": ");
+                var testResult = long.Parse(parts[0]);
+                var testValues = parts[1].Split(' ').Select(x => long.Parse(x)).ToList();
+
+                if (IsValidEquationWithConcats(testResult, null, testValues))
+                {
+                    totalCalibrationResult += testResult;
+                }
+            }
+
+            return totalCalibrationResult;
+        }
+
+        private bool IsValidEquation(long goal, long? resultSoFar, List<long> operands)
+        {
+            if (resultSoFar == null)
+            {
+                return IsValidEquation(goal, operands[0], operands.GetRange(1, operands.Count - 1));
+            }
+            else if (operands.Count == 1) {
+                return resultSoFar + operands[0] == goal || resultSoFar * operands[0] == goal;
+            }
+            else
+            {
+                return IsValidEquation(goal, resultSoFar + operands[0], operands.GetRange(1, operands.Count - 1)) ||
+                    IsValidEquation(goal, resultSoFar * operands[0], operands.GetRange(1, operands.Count - 1));
+            }
+        }
+
+        private bool IsValidEquationWithConcats(long goal, long? resultSoFar, List<long> operands)
+        {
+            if (resultSoFar == null)
+            {
+                return IsValidEquationWithConcats(goal, operands[0], operands.GetRange(1, operands.Count - 1));
+            }
+            else if (operands.Count == 1)
+            {
+                return resultSoFar + operands[0] == goal ||
+                    resultSoFar * operands[0] == goal ||
+                    Concat((long)resultSoFar, operands[0]) == goal;
+            }
+            else
+            {
+                return IsValidEquationWithConcats(goal, resultSoFar + operands[0], operands.GetRange(1, operands.Count - 1)) ||
+                    IsValidEquationWithConcats(goal, resultSoFar * operands[0], operands.GetRange(1, operands.Count - 1)) ||
+                    IsValidEquationWithConcats(goal, Concat((long)resultSoFar, operands[0]), operands.GetRange(1, operands.Count - 1));
+            }
+        }
+
+        private static long Concat(long a, long b)
+        {
+            return long.Parse(a.ToString() + b.ToString());
+        }
+
+    }
+}
---
+++ AdventOfCodeTests/2024/Day7Tests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using AdventOfCode._2024;
+using AdventOfCode;
+
+namespace AdventOfCodeTests
+{
+    public class Day7Tests
+    {
+        readonly Day7 day = new Day7();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        private static List<string> sampleInput = new List<string>
+        {
+            "190: 10 19",
+            "3267: 81 40 27",
+            "83: 17 5",
+            "156: 15 6",
+            "7290: 6 8 6 15",
+            "161011: 16 10 13",
+            "192: 17 8 14",
+            "21037: 9 7 18 13",
+            "292: 11 6 16 20",
+        };
+
+        private static readonly object[] inputTestCasesPart1 =
+        {
+            new object[]
+            {
+                sampleInput,
+                3749
+            },
+        };
+
+        private static readonly object[] inputTestCasesPart2 =
+        {
+            new object[]
+            {
+                sampleInput,
+                11387
+            },
+        };
+
+        [TestCaseSource("inputTestCasesPart1")]
+        public void TestPart1(List<string> input, int solution)
+        {
+            Assert.AreEqual((long)solution, day.Part1Long(input));
+        }
+
+        [TestCaseSource("inputTestCasesPart2")]
+        public void TestPart2(List<string> input, int solution)
+        {
+            Assert.AreEqual((long)solution, day.Part2Long(input));
+        }
+    }
+}
