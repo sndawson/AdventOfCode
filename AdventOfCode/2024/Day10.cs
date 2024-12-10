2c1f21104ca2a375443c956680609fabd479dafe
Shawna Dawson <sndawson@ualberta.ca>
2024-12-09 23:13:48 -0700

    Day 10

---
+++ AdventOfCode/2024/Day10.cs
@@ -0,0 +1,107 @@
+﻿namespace AdventOfCode._2024
+{
+    public class Day10 : IDay
+    {
+        public int Part1(List<string> input)
+        {
+            var sumOfTrailheadScores = 0;
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < input[0].Length; j++)
+                {
+                    if (input[i][j] == '0')
+                    {
+                        var trailHeadScore = DistinctReachableNines(input, (i, j)).Count;
+                        sumOfTrailheadScores += trailHeadScore;
+                    }
+                }
+            }
+            return sumOfTrailheadScores;
+        }
+
+        public int Part2(List<string> input)
+        {
+            var sumOfTrailheadScores = 0;
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < input[0].Length; j++)
+                {
+                    if (input[i][j] == '0')
+                    {
+                        var trailHeadScore = TotalReachableNines(input, (i, j)).Count;
+                        sumOfTrailheadScores += trailHeadScore;
+                    }
+                }
+            }
+            return sumOfTrailheadScores;
+        }
+
+        private static List<(int,int)> DistinctReachableNines(List<string> map, (int,int) currentPosition)
+        {
+            var reachableNines = new List<(int,int)> { };
+            var currentHeight = int.Parse(map[currentPosition.Item1][currentPosition.Item2].ToString());
+            if (currentHeight == 9)
+            {
+                reachableNines.Add(currentPosition);
+                return reachableNines;
+            }
+
+            var neighbours = GetNeighbours(currentPosition, map.Count, map[0].Length);
+            
+            foreach (var neighbour in neighbours)
+            {
+                var neighbourHeight = int.Parse(map[neighbour.Item1][neighbour.Item2].ToString());
+                if (neighbourHeight == currentHeight + 1)
+                {
+                    var reachableNinesFromNeighbour = DistinctReachableNines(map, neighbour);
+                    if (reachableNinesFromNeighbour.Count != 0)
+                    {
+                        reachableNines.AddRange(reachableNinesFromNeighbour);
+                    }
+                }
+            }
+
+            return reachableNines.Distinct().ToList();
+        }
+
+        private static List<(int, int)> TotalReachableNines(List<string> map, (int, int) currentPosition)
+        {
+            var reachableNines = new List<(int, int)> { };
+            var currentHeight = int.Parse(map[currentPosition.Item1][currentPosition.Item2].ToString());
+            if (currentHeight == 9)
+            {
+                reachableNines.Add(currentPosition);
+                return reachableNines;
+            }
+
+            var neighbours = GetNeighbours(currentPosition, map.Count, map[0].Length);
+
+            foreach (var neighbour in neighbours)
+            {
+                var neighbourHeight = int.Parse(map[neighbour.Item1][neighbour.Item2].ToString());
+                if (neighbourHeight == currentHeight + 1)
+                {
+                    var reachableNinesFromNeighbour = TotalReachableNines(map, neighbour);
+                    if (reachableNinesFromNeighbour.Count != 0)
+                    {
+                        reachableNines.AddRange(reachableNinesFromNeighbour);
+                    }
+                }
+            }
+
+            return reachableNines;
+        }
+
+        private static List<(int,int)> GetNeighbours((int,int) coordinate, int iMax, int jMax)
+        {
+            var i = coordinate.Item1;
+            var j = coordinate.Item2;
+            var neighbours = new List<(int, int)> { (i-1,j), (i,j-1), (i,j+1), (i+1,j) };
+            neighbours.RemoveAll(x => x.Item1 < 0 || x.Item1 >= iMax);
+            neighbours.RemoveAll(x => x.Item2 < 0 || x.Item2 >= jMax);
+
+            return neighbours;
+        }
+
+    }
+}
---
+++ AdventOfCodeTests/2024/Day10Tests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using AdventOfCode._2024;
+using AdventOfCode;
+
+namespace AdventOfCodeTests
+{
+    public class Day10Tests
+    {
+        readonly IDay day = new Day10();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        private static List<string> sampleInput1 = new List<string>
+        {
+            "0123",
+            "1234",
+            "8765",
+            "9876",
+        };
+
+        private static List<string> sampleInput2 = new List<string>
+        {
+            "89010123",
+            "78121874",
+            "87430965",
+            "96549874",
+            "45678903",
+            "32019012",
+            "01329801",
+            "10456732",
+        };
+
+        private static readonly object[] inputTestCasesPart1 =
+        {
+            new object[]
+            {
+                sampleInput1,
+                1
+            },
+            new object[]
+            {
+                sampleInput2,
+                36
+            },
+        };
+
+        private static readonly object[] inputTestCasesPart2 =
+        {
+            new object[]
+            {
+                sampleInput2,
+                81
+            },
+        };
+
+        [TestCaseSource("inputTestCasesPart1")]
+        public void TestPart1(List<string> input, int solution)
+        {
+            Assert.AreEqual(solution, day.Part1(input));
+        }
+
+        [TestCaseSource("inputTestCasesPart2")]
+        public void TestPart2(List<string> input, int solution)
+        {
+            Assert.AreEqual(solution, day.Part2(input));
+        }
+    }
+}
