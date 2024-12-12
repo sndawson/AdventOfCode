586b8abdd541f3591dc6240364b697370b2b17c1
Shawna Dawson <sndawson@ualberta.ca>
2024-12-11 21:27:52 -0700

    Day 11 Part 1
    Did this in 31 minutes yesterday but forgot to commit.

---
+++ AdventOfCode/2024/Day11.cs
@@ -0,0 +1,57 @@
+﻿namespace AdventOfCode._2024
+{
+    public class Day11 : IDay
+    {
+        public int Part1(List<string> input)
+        {
+            return (int)Part1(input, 25);
+        }
+
+        public static long Part1(List<string> input, int blinks)
+        {
+            // input should be one line
+            var initialStones = input[0];
+
+            var stones = initialStones.Split(' ').ToList();
+
+            var blinkCounter = 0;
+            while (blinkCounter < blinks)
+            {
+                Console.WriteLine($"Blink counter: {blinkCounter}");
+                for (int i = 0; i < stones.Count; i++)
+                {
+                    var stone = stones[i];
+                    if (stone == "0")
+                    {
+                        stones[i] = "1";
+                    }
+                    else if (stone.Length % 2 == 0)
+                    {
+                        var firstNewStone = stone.Substring(0, stone.Length / 2);
+                        var secondNewStone = long.Parse(stone.Substring(stone.Length / 2, stone.Length / 2)).ToString();
+                        stones[i] = firstNewStone;
+                        stones.Insert(i + 1, secondNewStone);
+                        i++;
+                    }
+                    else
+                    {
+                        var stoneValue = long.Parse(stone);
+                        stones[i] = (stoneValue * 2024).ToString();
+                    }
+                }
+                blinkCounter++;
+            }
+
+
+            return stones.Count;
+        }
+
+        public int Part2(List<string> input)
+        {
+            var result = Part1(input, 75);
+            Console.WriteLine(result);
+            return (int)result;
+        }
+
+    }
+}
---
+++ AdventOfCodeTests/2024/Day11Tests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using AdventOfCode._2024;
+using AdventOfCode;
+
+namespace AdventOfCodeTests
+{
+    public class Day11Tests
+    {
+        readonly Day11 day = new Day11();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        private static List<string> sampleInput1 = new List<string>
+        {
+            "0 1 10 99 999",
+        };
+
+        private static List<string> sampleInput2 = new List<string>
+        {
+            "125 17"
+        };
+
+        private static readonly object[] inputTestCasesPart1 =
+        {
+            new object[]
+            {
+                sampleInput1,
+                1,
+                7
+            },
+            new object[]
+            {
+                sampleInput2,
+                6,
+                22
+            },
+            new object[]
+            {
+                sampleInput2,
+                25,
+                55312
+            },
+        };
+
+        private static readonly object[] inputTestCasesPart2 =
+        {
+            new object[]
+            {
+                sampleInput2,
+                0
+            },
+        };
+
+        [TestCaseSource("inputTestCasesPart1")]
+        public void TestPart1(List<string> input, int blinks, int solution)
+        {
+            Assert.AreEqual(solution, day.Part1(input, blinks));
+        }
+
+        [TestCaseSource("inputTestCasesPart2")]
+        public void TestPart2(List<string> input, int solution)
+        {
+            Assert.AreEqual(solution, day.Part2(input));
+        }
+    }
+}
