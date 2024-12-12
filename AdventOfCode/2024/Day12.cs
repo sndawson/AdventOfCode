5fd4347dcbcadabdf99930d2c8a60df124c010b1
Shawna Dawson <sndawson@ualberta.ca>
2024-12-11 23:47:15 -0700

    Day 12 Part 1

---
+++ AdventOfCode/2024/Day12.cs
@@ -0,0 +1,105 @@
+﻿namespace AdventOfCode._2024
+{
+    public class Day12 : IDay
+    {
+        public int Part1(List<string> input)
+        {
+            var regions = GetRegions(input);
+            var price = GetPrice(input, regions);
+            return price;
+        }
+
+        public int Part2(List<string> input)
+        {
+            var regions = GetRegions(input);
+            var price = GetDiscountedPrice(input, regions);
+            return price;
+        }
+
+        private List<(char, List<(int, int)>)> GetRegions(List<string> input)
+        {
+            var regions = new List<(char, List<(int, int)>)>();
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    var plant = input[i][j];
+                    if (regions.Where(x => x.Item1 == plant && x.Item2.Contains((i, j))).Any())
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        var regionCoordinates = GetRegionCoordinates(input, (i, j), new List<(int, int)>());
+                        regions.Add((plant, regionCoordinates));
+                    }
+                }
+            }
+            return regions;
+        }
+
+        private int GetPrice(List<string> input, List<(char, List<(int, int)>)> regions)
+        {
+            var price = 0;
+            foreach (var region in regions)
+            {
+                var area = region.Item2.Count;
+                var perimeter = 0;
+                foreach (var regionCoordinate in region.Item2)
+                {
+                    var neighbours = GetNeighbours(regionCoordinate, input.Count, input[0].Length);
+                    var neighboursInRegion = neighbours.Where(x => input[x.Item1][x.Item2] == region.Item1).ToList();
+                    perimeter += 4 - neighboursInRegion.Count;
+                }
+                price += area * perimeter;
+            }
+
+            return price;
+        }
+
+        private int GetDiscountedPrice(List<string> input, List<(char, List<(int, int)>)> regions)
+        {
+            var price = 0;
+            foreach (var region in regions)
+            {
+                var area = region.Item2.Count;
+                var numberOfSides = 0;
+                // TODO
+            }
+
+            return price;
+        }
+
+        private List<(int,int)> GetRegionCoordinates(List<string> input, (int,int) startingCoord, List<(int,int)> regionCoords)
+        {
+            var plant = input[startingCoord.Item1][startingCoord.Item2];
+            var localRegionCoords = regionCoords.ToList();
+            localRegionCoords.Add(startingCoord);
+            var neighbours = GetNeighbours(startingCoord, input.Count, input[0].Length);
+            foreach (var neighbour in neighbours)
+            {
+                var neighbourValue = input[neighbour.Item1][neighbour.Item2];
+                if (neighbourValue == plant && !regionCoords.Contains(neighbour))
+                {
+                    var neighbourCoords = GetRegionCoordinates(input, neighbour, localRegionCoords);
+                    localRegionCoords.AddRange(neighbourCoords);
+                }
+            }
+
+            return localRegionCoords.Distinct().ToList();
+        }
+
+        // copy-pasted from day 10, maybe should add this to a utils class
+        private static List<(int, int)> GetNeighbours((int, int) coordinate, int iMax, int jMax)
+        {
+            var i = coordinate.Item1;
+            var j = coordinate.Item2;
+            var neighbours = new List<(int, int)> { (i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j) };
+            neighbours.RemoveAll(x => x.Item1 < 0 || x.Item1 >= iMax);
+            neighbours.RemoveAll(x => x.Item2 < 0 || x.Item2 >= jMax);
+
+            return neighbours;
+        }
+
+    }
+}
---
+++ AdventOfCodeTests/2024/Day12Tests.cs
@@ -0,0 +1,127 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using AdventOfCode._2024;
+using AdventOfCode;
+
+namespace AdventOfCodeTests
+{
+    public class Day12Tests
+    {
+        readonly IDay day = new Day12();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        private static List<string> sampleInput1 = new List<string>
+        {
+            "AAAA",
+            "BBCD",
+            "BBCC",
+            "EEEC",
+        };
+
+        private static List<string> sampleInput2 = new List<string>
+        {
+            "OOOOO",
+            "OXOXO",
+            "OOOOO",
+            "OXOXO",
+            "OOOOO"
+        };
+
+        private static List<string> sampleInput3 = new List<string>
+        {
+            "RRRRIICCFF",
+            "RRRRIICCCF",
+            "VVRRRCCFFF",
+            "VVRCCCJFFF",
+            "VVVVCJJCFE",
+            "VVIVCCJJEE",
+            "VVIIICJJEE",
+            "MIIIIIJJEE",
+            "MIIISIJEEE",
+            "MMMISSJEEE",
+        };
+
+        private static List<string> sampleInput4 = new List<string>
+        {
+            "EEEEE",
+            "EXXXX",
+            "EEEEE",
+            "EXXXX",
+            "EEEEE",
+        };
+
+        private static List<string> sampleInput5 = new List<string>
+        {
+            "AAAAAA",
+            "AAABBA",
+            "AAABBA",
+            "ABBAAA",
+            "ABBAAA",
+            "AAAAAA"
+        };
+
+        private static readonly object[] inputTestCasesPart1 =
+        {
+            new object[]
+            {
+                sampleInput1,
+                140
+            },
+            new object[]
+            {
+                sampleInput2,
+                772
+            },
+            new object[]
+            {
+                sampleInput3,
+                1930
+            },
+        };
+
+        private static readonly object[] inputTestCasesPart2 =
+        {
+            new object[]
+            {
+                sampleInput1,
+                80
+            },
+            new object[]
+            {
+                sampleInput2,
+                436
+            },
+            new object[]
+            {
+                sampleInput4,
+                236
+            },
+            new object[]
+            {
+                sampleInput5,
+                368
+            },
+            new object[]
+            {
+                sampleInput3,
+                1206
+            },
+        };
+
+        [TestCaseSource("inputTestCasesPart1")]
+        public void TestPart1(List<string> input, int solution)
+        {
+            Assert.AreEqual(solution, day.Part1(input));
+        }
+
+        [TestCaseSource("inputTestCasesPart2")]
+        public void TestPart2(List<string> input, int solution)
+        {
+            Assert.AreEqual(solution, day.Part2(input));
+        }
+    }
+}
