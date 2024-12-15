9796ee0d06562b2d49026bb9a88ddb1e9f42b9a8
Shawna Dawson <sndawson@ualberta.ca>
2024-12-14 23:56:18 -0700

    Day 15 Part 1

---
+++ AdventOfCode/2024/Day15.cs
@@ -0,0 +1,135 @@
+﻿namespace AdventOfCode._2024
+{
+    public class Day15 : IDay
+    {
+        private const char robotChar = '@';
+        private const char wallChar = '#';
+        private const char boxChar = 'O';
+        private const char blankChar = '.';
+
+        public int Part1(List<string> input)
+        {
+            var map = new List<List<char>>();
+            var parseMoves = false;
+            var moves = "";
+            var robot = (-1,-1);
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] != "" && !parseMoves)
+                {
+                    map.Add(new List<char>());
+                    for (int j = 0; j < input[i].Length; j++)
+                    {
+                        var charValue = input[i][j];
+                        map[i].Add(charValue);
+                        if (charValue == robotChar)
+                        {
+                            robot = (i, j);
+                        }
+                    }
+                }
+                else if (input[i] == "")
+                {
+                    parseMoves = true;
+                }
+                else
+                {
+                    moves += input[i];
+                }
+            }
+            PrintMap(map);
+
+            // run simulation
+            foreach (var move in moves)
+            {
+                (int,int) moveValue;
+                switch (move)
+                {
+                    case '^':
+                        moveValue = (-1, 0);
+                        break;
+                    case 'v':
+                        moveValue = (1, 0);
+                        break;
+                    case '<':
+                        moveValue = (0, -1);
+                        break;
+                    case '>':
+                        moveValue = (0, 1);
+                        break;
+                    default:
+                        throw new Exception("Invalid move");
+                }
+                var newRobotPos = TryToMoveChar(map, robot, moveValue);
+                robot = newRobotPos;  
+            }
+            PrintMap(map);
+
+            // calculate sum of box GPS coordinates
+            var gpsCoordsSum = 0;
+            for (int i = 0; i < map.Count; i++)
+            {
+                for (int j = 0; j < map[i].Count; j++)
+                {
+                    if (map[i][j] == boxChar)
+                    {
+                        var gpsCoord = (i * 100) + j;
+                        gpsCoordsSum += gpsCoord;
+                    }
+                }
+            }
+
+            return gpsCoordsSum;
+        }
+
+        public int Part2(List<string> input)
+        {
+            return 0;
+        }
+
+        private void PrintMap(List<List<char>> map)
+        {
+            Console.WriteLine("Map:");
+            for (int i = 0; i < map.Count; i++)
+            {
+                for (int j = 0; j < map[i].Count; j++)
+                {
+                    Console.Write(map[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private (int,int) TryToMoveChar(List<List<char>> map, (int,int) currentPos, (int,int) moveValue)
+        {
+            var currentCharValue = map[currentPos.Item1][currentPos.Item2];
+            var newPos = (currentPos.Item1 + moveValue.Item1, currentPos.Item2 + moveValue.Item2);
+            var mapAtNewPos = map[newPos.Item1][newPos.Item2];
+            switch (mapAtNewPos)
+            {
+                case blankChar:
+                    map[newPos.Item1][newPos.Item2] = currentCharValue;
+                    map[currentPos.Item1][currentPos.Item2] = blankChar;
+                    return newPos;
+                case wallChar:
+                    return currentPos;
+                case boxChar:
+                    // TODO: DRY
+                    var newBoxPos = TryToMoveChar(map, newPos, moveValue);
+                    if (newBoxPos != newPos)
+                    {
+                        map[newPos.Item1][newPos.Item2] = currentCharValue;
+                        map[currentPos.Item1][currentPos.Item2] = blankChar;
+                        return newPos;
+                    }
+                    else
+                    {
+                        return currentPos;
+                    }
+                default:
+                    throw new Exception("Invalid map char");
+            }
+        }
+
+    }
+}
---
+++ AdventOfCodeTests/2024/Day15Tests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using AdventOfCode._2024;
+using AdventOfCode;
+
+namespace AdventOfCodeTests
+{
+    public class Day15Tests
+    {
+        readonly IDay day = new Day15();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        private static List<string> sampleInput1 = new List<string>
+        {
+            "##########",
+            "#..O..O.O#",
+            "#......O.#",
+            "#.OO..O.O#",
+            "#..O@..O.#",
+            "#O#..O...#",
+            "#O..O..O.#",
+            "#.OO.O.OO#",
+            "#....O...#",
+            "##########",
+            "",
+            "<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+            "vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+            "><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+            "<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+            "^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+            "^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+            ">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+            "<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+            "^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+            "v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+        };
+
+        private static List<string> sampleInput2 = new List<string>
+        {
+            "########",
+            "#..O.O.#",
+            "##@.O..#",
+            "#...O..#",
+            "#.#.O..#",
+            "#...O..#",
+            "#......#",
+            "########",
+            "",
+            "<^^>>>vv<v>>v<<",
+        };
+
+        private static readonly object[] inputTestCasesPart1 =
+        {
+            new object[]
+            {
+                sampleInput2,
+                2028
+            },
+            new object[]
+            {
+                sampleInput1,
+                10092
+            },
+        };
+
+        private static readonly object[] inputTestCasesPart2 =
+        {
+            new object[]
+            {
+                sampleInput2,
+                0
+            },
+        };
+
+        [TestCaseSource("inputTestCasesPart1")]
+        public void TestPart1(List<string> input, int solution)
+        {
+            Assert.AreEqual(solution, day.Part1(input));
+        }
+
+        [TestCaseSource("inputTestCasesPart2")]
+        public void TestPart2(List<string> input, int solution)
+        {
+            Assert.AreEqual(solution, day.Part2(input));
+        }
+    }
+}
